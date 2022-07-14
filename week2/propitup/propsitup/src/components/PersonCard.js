d373e037f440fe365b1346f31900aaf3831fe9a0
Scott Nelson <scott.shannon.nelson@gmail.com>
2022-07-14 09:36:34 -0600

    putting it together commit

--- week2/propitup/propsitup/src/components/PersonCard.js
+++ week2/propitup/propsitup/src/components/PersonCard.js
@@ -1,12 +1,15 @@
-
+import React, {useState} from 'react';
 const PersonCard = (props) => {
     const {firstName,lastName,age,hColor} = props;
+    const [newAge,setAge] = useState(age)
+    const addAge = () => {setAge(newAge + 1)}
     return(
         <div>
             <h1>First Name: {firstName}</h1>
             <h3>Last Name: {lastName}</h3>
-            <h3>Age: {age}</h3>
+            <h3>Age: {newAge}</h3>
             <h3>Hair Color: {hColor}</h3>
+            <button onClick={addAge}>Birthday</button>
         </div>
 
     );
